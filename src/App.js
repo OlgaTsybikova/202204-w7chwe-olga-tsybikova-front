620db9aa2ecefcc01124ad4a131104089f5e7b88
Olga Tsybikova <tsy2701@gmail.com>
2022-05-22 12:54:12 +0200

    Added husky, sonar and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import LoginPage from "./pages/login/LoginPage";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
